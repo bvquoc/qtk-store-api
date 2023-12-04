6a8018ccd928f442b04758a18cb3010de21ee798
Quoc, Bui Vi <quoc.vi@codeforcevina.com>
2023-12-04 17:53:49 +0700

    update deploy script

--- go-deploy.sh
+++ go-deploy.sh
@@ -2,8 +2,8 @@
 
 echo "Building application"
 go build
-kill -15 $(pidof propq-search-service-v3-gin)
-#nohup ./propq-search-service-v3-gin > /dev/null 2>&1&
+kill -15 $(pidof qtk-store-api)
+#nohup ./qtk-store-api > /dev/null 2>&1&
 pm2 stop 0
-pm2 start propq-search-service-v3-gin
+pm2 start qtk-store-api
 echo "Deploy completed"
