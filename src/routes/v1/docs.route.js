af4eb21e8ce98a5a570edabc06dc2395c4ea8dde
Bui Vi Quoc <bvquoc.dev@gmail.com>
2023-12-27 17:24:52 +0700

    update

--- src/routes/v1/docs.route.js
+++ src/routes/v1/docs.route.js
@@ -11,12 +11,6 @@ const specs = swaggerJsdoc({
 });
 
 router.use('/', swaggerUi.serve);
-router.get(
-  '/',
-  swaggerUi.setup(specs, {
-    customJs: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-bundle.min.js',
-    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css',
-  })
-);
+router.get('/', swaggerUi.setup(specs));
 
 module.exports = router;
