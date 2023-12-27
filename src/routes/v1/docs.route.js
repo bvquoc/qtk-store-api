68f483f61550e5de5b933ae092379039c39c58be
Bui Vi Quoc <bvquoc.dev@gmail.com>
2023-12-27 17:12:36 +0700

    update

--- src/routes/v1/docs.route.js
+++ src/routes/v1/docs.route.js
@@ -14,15 +14,8 @@ router.use('/', swaggerUi.serve);
 router.get(
   '/',
   swaggerUi.setup(specs, {
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-bundle.min.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-standalone-preset.min.js',
-    ],
-    customCssUrl: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-standalone-preset.min.css',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.css',
-    ],
+    customJs: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui-bundle.min.js',
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.0.0/swagger-ui.min.css',
   })
 );
 
