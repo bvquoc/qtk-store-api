e1959011eddd2b37d8c3c2873dd19b7fda59953f
Bui Vi Quoc <bvquoc.dev@gmail.com>
2023-12-27 17:01:20 +0700

    update swagger version

--- src/routes/v1/docs.route.js
+++ src/routes/v1/docs.route.js
@@ -15,6 +15,15 @@ router.get(
   '/',
   swaggerUi.setup(specs, {
     explorer: true,
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
   })
 );
 
